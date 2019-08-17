87d10bf0da5e8e10ed1e063b25aed4e36b3f7bce
Mintae Kim <50645765+sonechca@users.noreply.github.com>
2019-08-17 18:57:23 -0400

    Add files via upload

---
+++ Workshop8_lab/SavingsAccount.h
@@ -0,0 +1,27 @@
+﻿/***********************************************************
+ // OOP244 Workshop 8: virtual Functions and Abstract Base Classes
+ // File: SavingsAccount.h
+ // Date: July 18, 2019
+ // Author: Hong Huang
+ // Name: Mintae Kim
+ // Reason: Workshop 8 lab
+ // Student#: 141915181
+ // E-mail: mkim221@myseneca.ca
+ ***********************************************************/
+
+#ifndef SICT_SAVINGSACCOUNT_H
+#define SICT_SAVINGSACCOUNT_H
+
+#include "Account.h"
+
+namespace sict {
+	class SavingsAccount : public Account {
+		double s_rate;
+	public:
+		SavingsAccount(double bal, double rate);
+		void monthEnd();
+		void display(std::ostream& os) const;
+	};
+}
+
+#endif /* SavingsAccount_hpp */
